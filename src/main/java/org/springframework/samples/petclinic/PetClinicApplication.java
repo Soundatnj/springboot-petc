63206ccd73c9d4ff273dcd7a8d591f4a6a9a2a05
DreamTeam <soundararajan.mani@gmail.com>
2017-12-05 15:16:12 +0530

    Update PetClinicApplication.java

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -19,7 +19,7 @@ package org.springframework.samples.petclinic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.context.annotation.ComponentScan;
 /**
  * PetClinic Spring Boot Application.
  * 
@@ -27,7 +27,7 @@ import org.springframework.context.annotation.ComponentScan;
  *
  */
 @SpringBootApplication
-@ComponentScan(basePackages="org.springframework.samples")
+//@ComponentScan(basePackages="org.springframework.samples")
 public class PetClinicApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) throws Exception {
